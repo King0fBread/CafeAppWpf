f93067fe5d279bb0e5c85335f1dfc684df379f5e
Nikita Glubokov <ITVenomancer@gmail.com>
2024-02-20 10:38:14 +0300

    Initial DB connection and some basic design

---
+++ CafeApp/CafeApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CafeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new LoginPage());
+        }
+
+        private void Orders_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Employees_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Shifts_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
